caae95c51c1fc6576cf695f5559a7ad8f011c52b
TheRockefelleR <84729968+TheRockefelleR@users.noreply.github.com>
2023-05-02 16:33:09 +0200

    Add files via upload

--- pluto.sh
+++ pluto.sh
@@ -4,3 +4,8 @@
 echo "Data utworzenia: $(date)" >> /logi/info.log
 echo "Dostepna pamiec: $(free -m) MB" >> /logi/info.log
 
+# Kod ktory dodalem by skrypt sie nie skonczyl
+#while true; do
+#  echo ""
+#  sleep 5
+#done
